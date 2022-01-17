96d15b0968ad8beb81a89afb81deb14357f1c334
SoftAnnaLee <jazzchamp600@gmail.com>
2022-01-17 11:22:52 -0700

    Change nvim's IndentBlankline plugin to lines rather than background highlighting

--- dot_config/nvim/plugins/indent-blank.lua
+++ dot_config/nvim/plugins/indent-blank.lua
@@ -1,18 +1,20 @@
-vim.cmd [[highlight IndentBlanklineIndent1 guibg=#353B49 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent2 guibg=#2E3440 gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent1 guibg=#353B49 gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent2 guibg=#2E3440 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#3b4252 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent2 guifg=#434c5e gui=nocombine]]
 
 require("indent_blankline").setup {
     use_treesitter = true,
     buftype_exclude = { "nofile", "help" },
     filetype_exclude = { "markdown", "vimwiki" },
-    char = "",
+    char = "│",
     char_highlight_list = {
         "IndentBlanklineIndent1",
         "IndentBlanklineIndent2",
     },
-    space_char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-    },
-    show_trailing_blankline_indent = false,
+    -- space_char_highlight_list = {
+    --     "IndentBlanklineIndent1",
+    --     "IndentBlanklineIndent2",
+    -- },
+    -- show_trailing_blankline_indent = false,
 }
